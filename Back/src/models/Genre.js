b24c1613c96ee15db9003440e92f18bff1d61db1
WagnerSmith <wagnersmith123@hotmail.com>
2023-10-23 13:33:13 -0500

    desarrollando la base de datos

---
+++ Back/src/models/Genre.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('videogame', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.ENUM("dadff","sdfadf","sdfad"),
+        allowNull: false
+    }
+  }, {timestamps: false});
+};
