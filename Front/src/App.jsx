560f736cf6efda8242742668ebce704bbbcc04bf
WagnerSmith <wagnersmith123@hotmail.com>
2023-11-05 15:18:05 -0500

    estilizando

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,15 +1,15 @@
-import { Route, Routes, useLocation } from 'react-router'
-import './App.css' 
-import HomePage from './Pages/HomePage';
+import { Route, Routes, useLocation } from 'react-router';
+import './App.css';
+import NavBar from './Components/Navbar';
 import DetailPage from './Pages/DetailPage';
 import FormPage from './Pages/FormPage';
-import NavBar from './Components/Navbar';
+import HomePage from './Pages/HomePage';
 import LandingPage from './Pages/LandingPage';
 
 function App() {
   const {pathname} = useLocation()
   return (
-    <>
+    <div className='App'>
       {pathname !== "/" && <NavBar/>}
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
@@ -17,7 +17,7 @@ function App() {
         <Route path="/detail/:id" element={<DetailPage/>}/>
         <Route path="/form" element={<FormPage/>}/>
       </Routes>
-    </>
+    </div>
   )
 }
 
