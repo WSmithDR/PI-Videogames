2172109c53972a367e5b109b957e758577744411
WagnerSmith <wagnersmith123@hotmail.com>
2023-11-05 17:09:13 -0500

    superado el navButton

--- Front/src/Components/NavButton.jsx
+++ Front/src/Components/NavButton.jsx
@@ -1,11 +1,17 @@
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { StyledButton } from "../Styles/NavButton";
 
-const NavButton = ({name, to}) => {
-    return(
-        <Link to={to}>
-            <button>{name}</button>
-        </Link>
-    )
-}
 
-export default NavButton
+const NavButton = ({ name, to }) => {
+  const location = useLocation();
+  const isCurrentRoute = location.pathname === to;
+
+  return (
+    <Link to={to}>
+      <StyledButton active={isCurrentRoute}>{name}</StyledButton>
+    </Link>
+  );
+};
+
+export default NavButton;
