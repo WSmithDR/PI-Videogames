3c65967a94feefec1a837a055f9d23527384b6d3
= <=>
2024-09-07 17:49:33 -0500

    allow access to any frontend

--- Back/src/app.js
+++ Back/src/app.js
@@ -14,8 +14,10 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+
+// Permitir acceso desde cualquier frontend (CORS)
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://videogamesback3-rslm2ly9.b4a.run'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // Permite acceso desde cualquier origen
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
